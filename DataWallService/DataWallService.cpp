df843b78964dd83d439ad0ab7244528595df6d07
JUSTSCIENCE0 <yakovmen62@gmail.com>
2021-05-02 00:47:29 +0700

    named pipe in service
    Changes to be committed:
    	modified:   DataWallService/DataWallService.cpp

--- DataWallService/DataWallService.cpp
+++ DataWallService/DataWallService.cpp
@@ -1,14 +1,54 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
-#define SERVICE_NAME  (LPSTR)("DataWallSrv")   
+#define SERVICE_NAME  (LPSTR)("DataWallService")   
+
+#define DEBUG
 
 #include <iostream>
 #include <Windows.h>
 #include <thread>
 #include <tchar.h>
 
+#ifdef DEBUG
+FILE* f_logs = fopen("D:\\serv_log.txt", "wb");
+void print_log(const char* _Format, ...)
+{
+    std::time_t t = std::time(0);
+    std::tm* now = std::localtime(&t);
+
+    va_list args;
+    va_start(args, _Format);
+    char buffer[1024];
+    memset(buffer, ' ', 20);
+    sprintf(buffer, "%02d.%02d.%04d %02d:%02d:%02d  ",
+        now->tm_mday,
+        now->tm_mon + 1,
+        now->tm_year + 1900,
+        now->tm_hour,
+        now->tm_min,
+        now->tm_sec
+    );
+    vsprintf(buffer + 20, _Format, args);
+    size_t size = std::strlen(buffer);
+    if (size < 1023)
+    {
+        buffer[size] = '\n';
+        size++;
+        buffer[size] = 0;
+    }
+    va_end(args);
+
+    fprintf(f_logs, buffer);
+    fflush(f_logs);
+}
+#else
+void print_log(const char* _Format, ...) {}
+#endif
+
 SERVICE_STATUS        g_ServiceStatus = { 0 };
 SERVICE_STATUS_HANDLE g_StatusHandle = NULL;
+HANDLE                hThread = INVALID_HANDLE_VALUE;
 HANDLE                g_ServiceStopEvent = INVALID_HANDLE_VALUE;
+HANDLE                hNamedPipe = INVALID_HANDLE_VALUE;
 
 VOID WINAPI ServiceMain(DWORD argc, LPTSTR* argv);
 VOID WINAPI ServiceCtrlHandler(DWORD);
@@ -30,16 +70,16 @@ START:
     // Tell the service controller we are starting
     ZeroMemory(&g_ServiceStatus, sizeof(g_ServiceStatus));
     g_ServiceStatus.dwServiceType = SERVICE_WIN32_OWN_PROCESS;
-    g_ServiceStatus.dwControlsAccepted = 0;
+    g_ServiceStatus.dwControlsAccepted = SERVICE_ACCEPT_STOP | SERVICE_ACCEPT_SHUTDOWN;
     g_ServiceStatus.dwCurrentState = SERVICE_START_PENDING;
     g_ServiceStatus.dwWin32ExitCode = 0;
     g_ServiceStatus.dwServiceSpecificExitCode = 0;
     g_ServiceStatus.dwCheckPoint = 0;
+    g_ServiceStatus.dwWaitHint = 0;
 
     if (SetServiceStatus(g_StatusHandle, &g_ServiceStatus) == FALSE)
     {
-        OutputDebugString(_T(
-            "My Sample Service: ServiceMain: SetServiceStatus returned error"));
+        print_log("Data Wall Service: ServiceMain: SetServiceStatus returned error");
     }
 
     /*
@@ -59,8 +99,7 @@ START:
 
         if (SetServiceStatus(g_StatusHandle, &g_ServiceStatus) == FALSE)
         {
-            OutputDebugString(_T(
-                "My Sample Service: ServiceMain: SetServiceStatus returned error"));
+            print_log("Data Wall Service: ServiceMain: SetServiceStatus returned error");
         }
         return;
     }
@@ -73,12 +112,11 @@ START:
 
     if (SetServiceStatus(g_StatusHandle, &g_ServiceStatus) == FALSE)
     {
-        OutputDebugString(_T(
-            "My Sample Service: ServiceMain: SetServiceStatus returned error"));
+        print_log("Data Wall Service: ServiceMain: SetServiceStatus returned error");
     }
 
     // Start a thread that will perform the main task of the service
-    HANDLE hThread = CreateThread(NULL, 0, ServiceWorkerThread, NULL, 0, NULL);
+    hThread = CreateThread(NULL, 0, ServiceWorkerThread, NULL, 0, NULL);
 
     if (hThread)
         WaitForSingleObject(hThread, INFINITE);
@@ -86,9 +124,8 @@ START:
         return;
 
     CloseHandle(g_ServiceStopEvent);
-
     //Service unstoppable
-    goto START;
+    //goto START;
 
     return;
 }
@@ -102,23 +139,26 @@ VOID WINAPI ServiceCtrlHandler(DWORD CtrlCode)
         if (g_ServiceStatus.dwCurrentState != SERVICE_RUNNING)
             break;
 
+        SetEvent(g_ServiceStopEvent);
+
+        if (hThread)
+            WaitForSingleObject(hThread, INFINITE);
         /*
          * Perform tasks necessary to stop the service here
          */
 
         g_ServiceStatus.dwControlsAccepted = 0;
-        g_ServiceStatus.dwCurrentState = SERVICE_STOP_PENDING;
+        g_ServiceStatus.dwCurrentState = SERVICE_STOPPED;
         g_ServiceStatus.dwWin32ExitCode = 0;
         g_ServiceStatus.dwCheckPoint = 4;
 
         if (SetServiceStatus(g_StatusHandle, &g_ServiceStatus) == FALSE)
         {
-            OutputDebugString(_T(
-                "My Sample Service: ServiceCtrlHandler: SetServiceStatus returned error"));
+            print_log("Data Wall Service: ServiceCtrlHandler: SetServiceStatus returned error");
         }
 
         // This will signal the worker thread to start shutting down
-        SetEvent(g_ServiceStopEvent);
+        print_log("SERVICE_CONTROL_STOP");
 
         break;
 
@@ -129,17 +169,37 @@ VOID WINAPI ServiceCtrlHandler(DWORD CtrlCode)
 
 DWORD WINAPI ServiceWorkerThread(LPVOID lpParam)
 {
+    hNamedPipe = CreateNamedPipe("\\\\.\\pipe\\datawallpipe",
+        PIPE_ACCESS_DUPLEX,
+        PIPE_TYPE_BYTE | PIPE_READMODE_BYTE | PIPE_WAIT,
+        1,
+        1024 * 16,
+        1024 * 16,
+        NMPWAIT_USE_DEFAULT_WAIT,
+        NULL);
+
+    if (hNamedPipe == INVALID_HANDLE_VALUE)
+    {
+        print_log("Data Wall Service: Start work: Failed to create named pipe");
+        return ERROR_PATH_NOT_FOUND;
+    }
+
+    print_log("Data Wall Service: Start work: Created named pipe");
+
     //  Periodically check if the service has been requested to stop
     while (WaitForSingleObject(g_ServiceStopEvent, 0) != WAIT_OBJECT_0)
     {
-        std::this_thread::sleep_for(std::chrono::minutes(1));
+        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
+    print_log("ServiceWorkerThread stopped");
     return ERROR_SUCCESS;
 }
 
 int main()
 {
+    print_log("Start");
+
     SERVICE_TABLE_ENTRY ServiceTable[] =
     {
         {SERVICE_NAME, (LPSERVICE_MAIN_FUNCTION)ServiceMain},
@@ -148,6 +208,11 @@ int main()
 
     if (StartServiceCtrlDispatcher(ServiceTable) == FALSE)
     {
+        print_log("StartServiceCtrlDispatcher(ServiceTable) == FALSE");
         return GetLastError();
     }
+
+    print_log("End");
+
+    return 0;
 }
